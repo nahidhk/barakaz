1eaff82b4d0946b87cfa998eb5eaae1331895241
NAHID HK <nahidappltd@gmail.com>
2026-04-12 00:50:10 +0600

    new update

--- admin/src/pages/Product.jsx
+++ admin/src/pages/Product.jsx
@@ -1,47 +1,49 @@
 import React from "react";
 import { useGetApi } from "../api/useGetApi";
-
+import api from "../api/api.json";
 
 export default function Product() {
-    const { jsonData: product } = useGetApi("products")
+    const { jsonData: product } = useGetApi("products");
+    const server = api.baseUrl + "uploads/";
     return (
         <>
             <div className="flex center medel w100">
-                <div className="flex center medel">
-                    <div className="table-container">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        #
-                                    </th>
-                                    <th>
-                                        Product Images
-                                    </th>
-                                    <th>
-                                        Product Name
-                                    </th>
-                                    <th>
-                                        Categore
-                                    </th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    product.map(item =>
-                                        <tr>
-                                            <td>{item.id}</td>
-                                            <td></td>
-                                            <td>{item.products_name}</td>
-                                            <td></td>
-                                        </tr>
-
-                                    )
-                                }
-                            </tbody>
-                        </table>
-                    </div>
+                <div className="table-container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    Product Images
+                                </th>
+                                <th>
+                                    Product Info
+                                </th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                product.map(item =>
+                                    <tr>
+                                        <td>{item.id}</td>
+                                        <td>
+                                            <img className="tableImg" src={server + item.products_image} />
+                                        </td>
+                                        <td>
+                                            <p><b>Name :</b> {item.products_name}</p>
+                                            <p><b>Category : </b> test1</p>
+                                            <p><b>Sub Category : </b>  test2</p>
+                                            <p><b>Old price : </b> 120Bdt</p>
+                                            <p><b>Price : </b> 123</p>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
